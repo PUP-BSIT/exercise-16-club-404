3c5881bc7fbf7e959bef29bf21d8e3b0bcbf1a27
delima_justine <delimajustine24@gmail.com>
2025-04-27 23:56:39 +0800

    Update files and website. (#10)

---
+++ delima_justine/php/index.php
@@ -0,0 +1,84 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $db = "exercise_16";
+  $conn = mysqli_connect($servername, $username, $password, $db);
+
+  if (!$conn) {
+    die("Connection not found.");
+  } else if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT * FROM song";
+    $result = mysqli_query($conn, $sql);
+    $response = [];
+
+    while($row = mysqli_fetch_assoc($result)) {
+    array_push($response, array(
+      'id'=>$row["id"],
+      'title'=> $row["title"],
+      'artist_name' => $row["artist_name"],
+      'year_released' => $row["year_released"],
+      'genre' => $row["genre"],
+      'album' => $row["album"]));
+    }
+
+    echo json_encode($response);
+  } else if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $artist = $_POST['artist_name'];
+    $year = $_POST['year_released'];
+    $genre = $_POST['genre'];
+    $album = $_POST['album'];
+    $sql = "INSERT INTO song (title, artist_name, year_released, genre, album)
+            VALUES ('$title', '$artist', '$year', '$genre', '$album')";
+    
+    if (!$title || !$artist || !$year || !$genre || !$album) {
+      die("Please fill all the song fields.");
+    }
+
+    if (!mysqli_query($conn, $sql)) {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    echo "New Song Added.";
+  } else if($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    parse_str(file_get_contents('php://input'), $_DELETE);
+
+    $id = $_DELETE["id"] ?? "";
+    $sql = "DELETE FROM song WHERE id=${id}";
+
+    if (!mysqli_query($conn, $sql)) {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    
+    echo "Deleted successfully!";
+  } else if($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+    parse_str(file_get_contents('php://input'), $_PATCH);
+
+    $id = $_PATCH["id"] ?? "";
+    $updatedTitle = $_PATCH["title"] ?? "";
+    $updatedArtist = $_PATCH["artist_name"] ?? ""; 
+    $updatedYear = $_PATCH["year_released"] ?? "";
+    $updatedGenre = $_PATCH["genre"] ?? "";
+    $updatedAlbum = $_PATCH["album"] ?? "";
+    $sql = "UPDATE song
+    SET title='${updatedTitle}', 
+      artist_name='${updatedArtist}', 
+      year_released='${updatedYear}', 
+      genre='${updatedGenre}',
+      album='${updatedAlbum}'
+    WHERE id=${id}";
+
+    if (!$id) {
+      die("Please select a song to update.");
+    }
+
+    if (!mysqli_query($conn, $sql)) {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    echo "Updated successfully";
+  }
+
+  mysqli_close($conn);
+?>
---
+++ delima_justine/scripts/song_script.js
@@ -0,0 +1,115 @@
+const songId = document.querySelector("#song_id");
+const songTitle = document.querySelector("#title");
+const songArtist = document.querySelector("#artist");
+const songYear = document.querySelector("#year_released");
+const songGenre = document.querySelector("#genre");
+const songAlbum = document.querySelector('#album');
+const endpoint = "https://localhost/index.php";
+
+// Function to insert a new song into the database/table
+function insertSong() {
+  fetch("./index.php", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `title=${songTitle.value}&artist_name=${songArtist.value}&` +
+          `year_released=${songYear.value}&genre=${songGenre.value}&` +
+          `album=${songAlbum.value}`
+  })
+  .then((response) => response.text())
+  .then((responseText) => {
+    alert(responseText);
+    displayData();
+  });
+}
+
+// Function to delete a song from the database/table
+function deleteSong(id) {
+  fetch("./index.php", {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `id=${id}`,
+  })
+  .then((response) => response.text())
+  .then((responseText) => {
+    alert(responseText);
+    displayData();
+  })
+}
+
+// Sends the song data to the edit form
+function editSong(id, title, artist, year, genre, album) {
+  songId.value = id;
+  songTitle.value = title;
+  songArtist.value = artist;
+  songYear.value = year;
+  songGenre.value = genre;
+  songAlbum.value = album;
+}
+
+function clearSongForm() {
+  songId.value = "";
+  songTitle.value = "";
+  songArtist.value = "";
+  songYear.value = "";
+  songGenre.value = "";
+  songAlbum.value = "";
+}
+
+// Function to update a song in the database/table
+function updateSong() {
+  fetch("./index.php", {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `id=${songId.value}&title=${songTitle.value}&` +
+          `artist_name=${songArtist.value}&year_released=${songYear.value}&` +
+          `genre=${songGenre.value}&album=${songAlbum.value}`
+  })
+  .then((response) => response.text())
+  .then((responseText) => {
+    if(!songId) {
+      alert("Please select a song id to update.");
+      return;
+    }
+
+    alert(responseText);
+    displayData();
+  })
+}
+
+// Function to display all songs to the user interface/HTML table
+function displayData() {
+  const tableBody = document.querySelector("#container");
+  fetch("./index.php")
+  .then((response) => response.json())
+  .then((songList) => {
+    tableBody.innerHTML = "";
+
+    for (const song of songList) {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+      <td>${song.id}</td>
+      <td>${song.title}</td>
+      <td>${song.artist_name}</td>
+      <td>${song.year_released}</td>
+      <td>${song.genre}</td>
+      <td>${song.album}</td>
+      <td><button onClick="editSong(
+        ${song.id}, 
+        '${song.title}', 
+        '${song.artist_name}', 
+        '${song.year_released}', 
+        '${song.genre}',
+        '${song.album}')">Edit</button></td>
+      <td><button onClick="deleteSong(${song.id})">Delete</button></td>`
+      tableBody.append(row);
+    }
+   });
+}
+
+displayData();
